7823125ea3877991ab5eafc9d07ca9b90312cdcd
Arlind <arlindhalimi1604@gmail.com>
2026-03-22 01:13:40 +0100

    update footer content and layout with synced brand   logos

--- themes/bsn-racing/footer.php
+++ themes/bsn-racing/footer.php
@@ -1,16 +1,21 @@
+<?php $bsn_footer_brand_links = function_exists('bsn_racing_get_navbar_brands') ? bsn_racing_get_navbar_brands() : []; ?>
+
 <footer class="site-footer">
   <div class="bsn-container">
     <div class="site-footer__grid">
       <div>
-        <p class="site-footer__eyebrow">BSN-RACING</p>
-        <p>Am Espen 27<br>90559 Oberferrieden<br>Germany</p>
-        <p>Tel.: +49 (0) 9183 9029 494<br>Mail: info@bsn-racing.de</p>
+        <p class="site-footer__eyebrow">Wann wir fuer Sie da sind</p>
+        <p>Sonntag - Montag:</p>
+        <p>Dienstag - Freitag:</p>
+        <p>Samstag:</p>
+        <p>Geschlossen<br>9:00-12:00 13:30-18:30<br>Nach Vereinbarung</p>
       </div>
 
       <div>
-        <p class="site-footer__eyebrow">Oeffnungszeiten</p>
-        <p>Mo-Fr<br>10:00 - 18:00</p>
-        <p>Sa<br>10:00 - 13:00</p>
+        <p class="site-footer__eyebrow">Unsere Adresse</p>
+        <p>Off Road Shop Metzler GmbH<br>Zuercherstrasse 330<br>8500 Frauenfeld<br>CH Schweiz</p>
+        <p class="site-footer__eyebrow">Wie Sie uns erreichen</p>
+        <p>Telefon: +41 52 721 47 08<br>E-Mail: info@offroadshop-metzler.com</p>
       </div>
 
       <div>
@@ -40,10 +45,29 @@
     </div>
 
     <div class="site-footer__bottom">
-      <p>&copy; <?php echo esc_html(date_i18n('Y')); ?> bsn-racing.de</p>
-      <div class="site-footer__legal">
-        <a href="<?php echo esc_url(home_url('/datenschutz/')); ?>">Datenschutz</a>
-        <a href="<?php echo esc_url(home_url('/impressum/')); ?>">Impressum</a>
+      <?php if (!empty($bsn_footer_brand_links)) : ?>
+        <div class="site-footer__brands" aria-label="<?php esc_attr_e('Partner brands', 'bsn-racing'); ?>">
+          <?php foreach ($bsn_footer_brand_links as $brand) : ?>
+            <a class="site-footer__brand" href="<?php echo esc_url($brand['url']); ?>" target="_blank" rel="noreferrer noopener">
+              <img
+                src="<?php echo esc_url($brand['image_url']); ?>"
+                alt="<?php echo esc_attr($brand['label']); ?>"
+                loading="lazy"
+              >
+            </a>
+          <?php endforeach; ?>
+        </div>
+      <?php endif; ?>
+
+      <div class="site-footer__bottom-right">
+        <div class="site-footer__legal">
+          <a href="<?php echo esc_url(home_url('/datenschutz/')); ?>">Datenschutz</a>
+          <a href="<?php echo esc_url(home_url('/impressum/')); ?>">Impressum</a>
+        </div>
+        
+      </div>
+      <div class="site-footer__copyright">
+        <p>&copy; <?php echo esc_html(date_i18n('Y')); ?> offroadshop-metzler.com</p>
       </div>
     </div>
   </div>
